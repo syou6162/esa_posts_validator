6afdbf31a0e86afea35b062264c0a99b13fc8d31
Yasuhisa Yoshida <syou6162@gmail.com>
2021-03-12 20:31:24 +0900

    カテゴリが付いているかvalidation

---
+++ src/category_validator.ts
@@ -0,0 +1,44 @@
+import { AxiosInstance } from 'axios';
+
+const { IncomingWebhook } = require('@slack/webhook');
+
+import { EsaConfig, EsaPost, EsaSearchResult, getEsaConfig, createAxiosClient } from './esa'
+
+async function getPostsWithNoCategory(
+    axios: AxiosInstance,
+    esaConfig: EsaConfig,
+): Promise<EsaSearchResult> {
+    const response = await axios.get<EsaSearchResult>(`/v1/teams/${esaConfig.teamName}/posts`, {
+        params: {
+            q: "on:/",
+            sort: "number",
+            order: "asc"
+        },
+    });
+    return response.data;
+}
+
+const esaConfig = getEsaConfig();
+const axios = createAxiosClient(esaConfig.accessToken);
+
+getPostsWithNoCategory(axios, esaConfig).then((result: EsaSearchResult) => {
+    return result.posts.filter((post: EsaPost) => {
+        return post.category == null && post.name !== "README.md"
+    })
+}).then((posts: EsaPost[]) => {
+    const env = process.env
+    const url = env.SLACK_WEBHOOK || ""
+    const webhook = new IncomingWebhook(url);
+    if (posts.length > 0) {
+        let text = "以下のpostのカテゴリを考えませんか?\n"
+        posts.slice(0, 3).forEach((post: EsaPost) => {
+            text += `- ${post.full_name}\n`;
+            text += `  - https://${esaConfig.teamName}.esa.io/posts/${post.number}\n`
+        });
+        (async () => {
+            await webhook.send({
+                text: text
+            });
+        })();
+    }
+});
